4f5fc94488a0c08679337ef84c1235e7026cf695
David Johns <david.johns@icewire.ca>
2021-02-25 15:09:37 -0500

    updated names and directory in buildExecutables

--- buildExecutables.sh
+++ buildExecutables.sh
@@ -1,18 +1,16 @@
 #!/bin/sh
 # to run this shell script use: bash buildExecutables.sh
 #
-# Script is used to build and zip icemaker versions
-date=_200805
-env GOOS=windows GOARCH=amd64 go build -o icemaker.exe
-zip -r icemakerWin64$date.zip icemaker.exe
-rm icemaker.exe
-env GOOS=windows GOARCH=386 go build -o icemaker.exe
-zip -r icemakerWin32$date.zip icemaker.exe
-rm icemaker.exe
-env GOOS=linux GOARCH=amd64 go build -o icemaker
-zip -r icemakerLinux64$date.zip icemaker
-rm icemaker
-env GOOS=darwin GOARCH=amd64 go build -o icemaker
-zip -r icemakerMacOS64$date.zip icemaker
-rm icemaker
+# Script is used to build and zip ltspice2svg versions
+date=_210225
+env GOOS=windows GOARCH=amd64 go build -o binaries/ltspice2svg.exe
+zip -r binaries/ltspice2svgWin64$date.zip binaries/ltspice2svg.exe
+rm binaries/ltspice2svg.exe
+env GOOS=linux GOARCH=amd64 go build -o binaries/ltspice2svg
+zip -r binaries/ltspice2svgLinux64$date.zip binaries/ltspice2svg
+rm binaries/ltspice2svg
+env GOOS=darwin GOARCH=amd64 go build -o binaries/ltspice2svg
+zip -r binaries/ltspice2svgMacOS64$date.zip binaries/ltspice2svg
+rm binaries/ltspice2svg
+
 
