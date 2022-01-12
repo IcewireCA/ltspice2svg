d7430e11672a3619e870771569f393c118587a2a
David Johns <david.johns@icewire.ca>
2022-01-12 11:45:15 -0500

    remove files bugList.txt buildExecutables.sh runBackup.sh

--- runBackup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# shell script to backup this directory but ignore node_modules
-
-rsync -av --progress /Users/davidjohns/GitHub/go/src/ltspice2svg /Users/davidjohns/Dropbox/Icewire/git
