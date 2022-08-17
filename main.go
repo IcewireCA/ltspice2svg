013a3f486fc5797a0dbe52b7bf968723e259e031
David Johns <david.johns@icewire.ca>
2022-08-17 15:57:02 -0400

    update version info

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ func main() {
 
 	//	todayDate = currentTime.Format("2006-01-02")
 	// ENTER NEW VERSION NUMBER/DATE HERE
-	version = "0.8.2" + " (" + "2021-02-09" + ")"
+	version = "0.8.2" + " (" + "2022-08-17" + ")"
 
 	inFile, outFile, symPath, txtMode, dotsMode, fontType, logOut = commandFlags(version) // outFile depends on inFile file extension
 	fileWriteString("", outFile.full)
